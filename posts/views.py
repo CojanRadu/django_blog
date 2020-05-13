0e60634a657f0ec342c9b3aaac16cd851bdda2d4
CojanRadu <raduacojan@gmail.com>
2020-05-13 14:14:07 +0100

    Added heroku and postgres

---
+++ posts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import BlogPostForm
+
+def get_posts(request):
+    """ create a view that will return a list of posts """
+    """ will render to blogposts.html """
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blogposts.html', {'posts':posts})
+
+def post_detail(request, pk):
+    """ create a view that returns a single post """
+
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+    return render(request, 'postdetail.html', {'post':post})
+
+def create_or_edit_post(request, pk=None):
+    """ create a view to edit or add apost """    
+
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post_detail, post.pk)
+        else:
+            print("formnot")
+    else:
+        form =BlogPostForm(instance=post)
+    return render(request, 'blogpostform.html', {'form':form})       
